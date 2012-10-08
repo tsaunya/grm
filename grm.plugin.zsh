c35d5a1c64801b2ddc0b6c3a2378b23b06c5968b
Asp1d (Yanis Tsaunya) <yanis88@gmail.com>
2012-10-08 15:55:09 +0400

    added merge

--- grm.plugin.zsh
+++ grm.plugin.zsh
@@ -4,6 +4,8 @@ function grm() {
   if [ "x$1" != "x" ]; then
     local current=$(git rev-parse --abbrev-ref HEAD)
     local origin=$1
-    echo $(git checkout $origin && git pull origin $origin && git checkout $current && git rebase -p $origin)
+    if [ "$current" != "$origin" ]; then
+      echo $(git checkout $origin && git pull origin $origin && git checkout $current && git rebase -p $origin && git checkout $origin && git merge --no-ff $current)
+    fi
   fi
 }
