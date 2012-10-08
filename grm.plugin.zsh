0019a3ff24235b98bc7494020688d61885abf6d4
Asp1d (Yanis Tsaunya) <yanis88@gmail.com>
2012-10-08 18:10:19 +0400

    added merge of remote branch

--- grm.plugin.zsh
+++ grm.plugin.zsh
@@ -2,10 +2,20 @@
 
 function grm() {
   if [ "x$1" != "x" ]; then
-    local current=$(git rev-parse --abbrev-ref HEAD)
-    local origin=$1
+    origin=$(git rev-parse --abbrev-ref HEAD)
+    echo $origin
+    current=$1
+    echo $current
     if [ "$current" != "$origin" ]; then
-      echo $(git checkout $origin && git pull origin $origin && git checkout $current && git rebase -p $origin && git checkout $origin && git merge --no-ff $current)
+      git pull origin $origin
+      git checkout $current
+      co=$(git rev-parse --abbrev-ref HEAD)
+      if [[ $co != $current ]]; then
+        git checkout -b $current origin/$current
+      fi
+      git rebase -p $origin
+      git checkout $origin
+      git merge --no-ff $current
     fi
   fi
 }
